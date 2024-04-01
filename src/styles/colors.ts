317e326cb074c770df89ddc0498086a242a622c4
Hypatia Apaestegui <patyapaesteguicorp@gmail.com>
2024-04-01 10:58:44 -0500

    refactor colors file

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -9,31 +9,71 @@ export const colors = {
     main: '#fff',
     disabled: '#ccc',
   },
+
   primary: {
     main: '#f83d48',
-    disabled: '#FFC7CA',
-    hover: '#e61c28',
-    focus: '#C1141E',
+    hover: {
+      300: '#FFA0A6',
+      400: '#FF6A73',
+      500: '#F83D48',
+    },
+    active: {
+      600: '#E61C28',
+    },
+    disabled: {
+      100: '#FFE1E3',
+      400: '#FF6A73',
+    },
+    focus: {
+      500: '#F83D48',
+    },
   },
   secondary: {
     main: '#2d2d2d',
-    disabled: '#424242',
-    selected: '#b0b0b0',
-    hover: '#5d5d5d',
-    focus: '#4f4f4f',
+    disabled: {
+      100: '#E7E7E7',
+      400: '#D1D1D1',
+    },
+    hover: {
+      300: '#B0B0B0',
+      400: '#888888',
+      500: '#6D6D6D',
+    },
+    active: {
+      600: '#5D5D5D',
+    },
+    focus: {
+      500: '#6D6D6D',
+    },
+  },
+  tertiary: {
+    main: '#F8F6FC',
+    disabled: {
+      100: '#F1EEF9',
+      400: '#B6A2DE',
+    },
+    hover: {
+      300: '#D3C7EC',
+      400: '#B6A2DE',
+      500: '#997DCF',
+    },
+    active: {
+      600: '#8261BC',
+    },
+    focus: {
+      500: '#997DCF',
+    },
   },
   red: {
-    50: '#FFF0F3',
-    100: '#FFDEE4',
-    200: '#FFC2CC',
-    300: '#FF98A9',
-    400: '#FF5C78',
-    500: '#FF2A4E',
-    600: '#F80A32',
-    700: '#D20427',
-    800: '#AC0824',
-    900: '#8E0E24',
-    950: '#4E010E',
-    1000: '#f83d48',
+    50: '#FFF1F2',
+    100: '#FFE1E3',
+    200: '#FFC7CA',
+    300: '#FFA0A6',
+    400: '#FF6A73',
+    500: '#F83D48',
+    600: '#E61C28',
+    700: '#C1141E',
+    800: '#A0141C',
+    900: '#84181E',
+    950: '#48070B',
   },
-}
