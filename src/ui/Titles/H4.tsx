39d6bf1e3bec2af03fb2e196ad517d56bd5b65a5
CrhistianT <andrewturpo573@gmail.com>
2024-04-17 03:14:05 -0500

    fix: font weight setting corrected

--- src/ui/Titles/H4.tsx
+++ src/ui/Titles/H4.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components"
 
+import { FontWeight } from "styles/typography"
 import { IH4Props } from "types/ui/Titles"
 
 const StyledH4 = styled.h4<IH4Props>`
-  font-weight: ${(props) => props.weight};
+  font-weight: ${(props) => FontWeight[props.weight]};
   font-size: clamp(1.125rem, 0.75rem + 1.2vw, 1.5rem);
   line-height: 1.5;
 `
