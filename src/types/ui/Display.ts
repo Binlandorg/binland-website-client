581980ef2fb063072cf57fe3154dfdd45c22981a
Gustavout22 <gustavougartetorres@gmail.com>
2024-05-01 12:42:51 -0500

    fix: display component

--- src/types/ui/Display.ts
+++ src/types/ui/Display.ts
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react'
+import { WithClassName } from 'types/components/shared'
 
-export interface IDisplayProps extends PropsWithChildren {
+export interface IDisplayProps extends PropsWithChildren, WithClassName {
   fontStyle?: 'normal' | 'italic' | 'oblique'
   size: 'sm' | 'md' | 'lg'
 }
--- src/ui/Display/Display.tsx
+++ src/ui/Display/Display.tsx
@@ -1,9 +1,14 @@
 import { IDisplayProps } from 'types/ui/Display'
 import { StyledDisplay } from './Display.styles'
 
-const Display: React.FC<IDisplayProps> = ({ size, fontStyle, children }) => {
+const Display: React.FC<IDisplayProps> = ({
+  size,
+  fontStyle,
+  children,
+  className,
+}) => {
   return (
-    <StyledDisplay size={size} fontStyle={fontStyle}>
+    <StyledDisplay size={size} fontStyle={fontStyle} className={className}>
       {children}
     </StyledDisplay>
   )
