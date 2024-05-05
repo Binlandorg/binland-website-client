e006841f3409c852663c67e8ad3ab7fc2502313a
Jonathan Alarcón <mardecera.personal@gmail.com>
2024-05-04 22:07:55 -0500

    Now a function is used that returns the current year for the copyright. (#115)

--- src/components/Footer/Footer.data.tsx
+++ src/components/Footer/Footer.data.tsx
@@ -7,11 +7,10 @@ interface IFooterLinks {
 export const legalLinks: IFooterLinks[] = [
   { id: '1', to: '/terms-of-service', name: 'footer.terms.of.service' },
   { id: '2', to: '/privacy-policy', name: 'footer.privacy.policy' },
-  { id: '3', to: '', name: 'footer.conditions.of.use' },
+  { id: '3', to: '/', name: 'footer.conditions.of.use' },
 ]
 
 export const enterpriseLinks: IFooterLinks[] = [
   { id: '1', to: '/about-us', name: 'footer.about.us' },
-  { id: '2', to: '/portafolio', name: 'footer.projects' },
   { id: '3', to: '/frequently-asked-question', name: 'footer.FAQ' },
 ]
