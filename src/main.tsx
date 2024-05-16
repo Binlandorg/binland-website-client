4f43d3af2a8cd1e5683c3e1c109abc1c6a8bce8e
Crhistian Turpo <65095486+CrhistianT7@users.noreply.github.com>
2024-05-16 13:44:47 -0500

    refactor: remove prismic library (#136)
    * refactor: remove prismic library
    
    * chore: remove pmpn yaml file

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,13 @@ import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { client } from 'prismicio/prismicio'
-import { PrismicProvider } from '@prismicio/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <PrismicProvider client={client}>
         <LanguageProvider>
           <App />
         </LanguageProvider>
-      </PrismicProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
