0dc7759282cebf5f6085edd87e658eda99e28e0f
CrhistianT <andrewturpo573@gmail.com>
2024-05-17 21:44:51 -0500

    fix: rename component ContactUsForm

--- src/pages/ContactUs/ContactUs.tsx
+++ src/pages/ContactUs/ContactUs.tsx
@@ -5,7 +5,7 @@ import {
   SectionContactUs,
 } from 'components/ContactUs/ContactUs.styles'
 import ContactUsInformation from 'components/ContactUs/ContactUsInformation/ContactUsInformation'
-import FormContacUs from 'components/ContactUs/ContactUsForm/ContactUsForm'
+import ContactUsForm from 'components/ContactUs/ContactUsForm/ContactUsForm'
 import useIntlMessages from 'hooks/useIntlMessages'
 import H1 from 'ui/Titles/H1'
 
@@ -23,7 +23,7 @@ const ContactUs: React.FC = () => {
         <ContactUsWrapper size="xl">
           <ContentWrapper>
             <ContactUsInformation />
-            <FormContacUs />
+            <ContactUsForm />
           </ContentWrapper>
         </ContactUsWrapper>
       </SectionContactUs>
