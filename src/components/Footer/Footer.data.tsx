8cb9e3db73d39a71a914747730ca1e96e9aa6f70
Gustavout22 <gustavougartetorres@gmail.com>
2024-07-27 09:46:06 -0500

    fix: enterprise links iin footer data

--- src/components/Footer/Footer.data.tsx
+++ src/components/Footer/Footer.data.tsx
@@ -11,5 +11,5 @@ export const legalLinks: IFooterLinks[] = [
 
 export const enterpriseLinks: IFooterLinks[] = [
   { id: '1', to: '/about-us', name: 'footer.about.us' },
-  { id: '3', to: '/frequently-asked-question', name: 'footer.FAQ' },
+  { id: '2', to: '/frequently-asked-question', name: 'footer.FAQ' },
 ]
