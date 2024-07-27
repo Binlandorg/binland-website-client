7dd2a9a3ac540091ae14fb0a9f96f0a686c861b2
Gustavo Ugarte <124841482+GustavoUT22@users.noreply.github.com>
2024-07-27 09:48:11 -0500

    quit conditions of use (#154)
    * quit conditions of use
    
    * fix: enterprise links iin footer data

--- src/components/Footer/Footer.data.tsx
+++ src/components/Footer/Footer.data.tsx
@@ -7,10 +7,9 @@ interface IFooterLinks {
 export const legalLinks: IFooterLinks[] = [
   { id: '1', to: '/terms-of-service', name: 'footer.terms.of.service' },
   { id: '2', to: '/privacy-policy', name: 'footer.privacy.policy' },
-  { id: '3', to: '/', name: 'footer.conditions.of.use' },
 ]
 
 export const enterpriseLinks: IFooterLinks[] = [
   { id: '1', to: '/about-us', name: 'footer.about.us' },
-  { id: '3', to: '/frequently-asked-question', name: 'footer.FAQ' },
+  { id: '2', to: '/frequently-asked-question', name: 'footer.FAQ' },
 ]
