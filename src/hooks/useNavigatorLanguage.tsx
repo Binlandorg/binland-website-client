ead98ceca0f3bdcc493d937647bffdc64c652d6c
mardecera <mardecera.personal@gmail.com>
2024-09-04 22:45:02 -0500

    task: delete example of custom hook

--- src/hooks/useNavigatorLanguage.tsx
+++ src/hooks/useNavigatorLanguage.tsx
@@ -11,14 +11,6 @@ const getLang = () => navigator.language.split('-')[0]
  * Hook that gets the current language used by the browser.
  * 
  * @returns string Language: 2-letter format without its regional variations.
- * 
- * @example
- * ```ts
- * import useNavigatorLanguage from '@/hooks/useNavigatorLanguage';
- * 
- * const navLanguage = useNavigatorLanguage()
- * console.log(navLanguage)
- * ```
  */
 
 export const useNavigatorLanguage = (): string =>
