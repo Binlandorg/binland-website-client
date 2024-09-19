c461fa4ab0ae937374043cca635715f054796703
Jhon <mardecera.personal@gmail.com>
2024-09-18 20:35:26 -0500

    refactor: reorder social media buttons (#171)

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -23,15 +23,15 @@ const socialLinksIcons: ILink[] = [
   },
   {
     id: '2',
-    to: 'www.instagram.com/binland.dev',
-    icon: <FaInstagram size={ICON_SIZE} />,
-    ariaLabel: 'home.hero.social.media.instagram.aria.label',
-  },
-  {
-    id: '3',
     to: 'www.linkedin.com/company/binland',
     icon: <FaLinkedinIn size={ICON_SIZE} />,
     ariaLabel: 'home.hero.social.media.linkedin.aria.label',
+  },
+  {
+    id: '3',
+    to: 'www.instagram.com/binland.dev',
+    icon: <FaInstagram size={ICON_SIZE} />,
+    ariaLabel: 'home.hero.social.media.instagram.aria.label',
   },
   {
     id: '4',
