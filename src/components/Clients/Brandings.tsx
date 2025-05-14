a2f5e34795a37f2c27f85848d2ad140bd46b532e
mardecera <18353569+mardecera@users.noreply.github.com>
2025-05-14 14:04:06 -0500

    feat: add new isotypes to the clients section

--- src/components/Clients/Brandings.tsx
+++ src/components/Clients/Brandings.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
 import { LazyLoadImage } from "react-lazy-load-image-component"
 
+import agroAmym from "./../../assets/images/brandings/agro-amym-logo.svg"
 import naturalEcoFriendly from "./../../assets/images/brandings/naturaleza-ecoamigable-logo.svg"
 import oasisRestaurant from "./../../assets/images/brandings/oasis-logo.webp"
+import pollosControl from "./../../assets/images/brandings/pollos-control-logo.svg"
 import { BrandingsWrapper } from "./Clients.styles"
 
 interface Brand {
@@ -13,6 +15,8 @@ interface Brand {
 const Brands: Brand[] = [
 	{ id: 1, src: naturalEcoFriendly, name: "naturaleza eco-amigable" },
 	{ id: 2, src: oasisRestaurant, name: "Oasis restaurant" },
+	{ id: 3, src: pollosControl, name: "Pollos Control" },
+	{ id: 4, src: agroAmym, name: "Agro Amym" },
 ]
 
 const Brandings: React.FC = () => {
--- src/ui/Icons/Check.tsx
+++ src/ui/Icons/Check.tsx
@@ -8,8 +8,8 @@ const Check = () => {
 			aria-label="check"
 		>
 			<path
-				fill-rule="evenodd"
-				clip-rule="evenodd"
+				fillRule="evenodd"
+				clipRule="evenodd"
 				d="M19.546 1.111a1.5 1.5 0 0 1 0 2.121L8.303 14.475a1.6 1.6 0 0 1-2.263 0L.454 8.89a1.5 1.5 0 1 1 2.121-2.121l4.596 4.596L17.424 1.111a1.5 1.5 0 0 1 2.122 0Z"
 				fill="#14BC39"
 			/>
