81ee500cd0e8eee8ab1b0b907347f57aa6b3e4a7
mardecera <18353569+mardecera@users.noreply.github.com>
2025-06-23 12:38:37 -0500

    feat: implement redesign of clients section on home page

--- src/components/Clients/Clients.styles.ts
+++ src/components/Clients/Clients.styles.ts
@@ -10,7 +10,7 @@ export const ClientsSection = styled(Section)`
 	background-color: ${colors.white.main};
 	width: calc(100% + var(--padding-section-x));
 	max-width: inherit;
-	padding: 3.75rem 1.5rem ;
+	padding: 3.75rem 1.5rem;
 	position: relative;
 	left: -1.5rem;
 
@@ -18,7 +18,7 @@ export const ClientsSection = styled(Section)`
 		--padding-section-x: calc(5rem * 2);
 		width: calc(100% + var(--padding-section-x));
 		left: -5rem;
-		padding: 6.25rem 5rem ;
+		padding: 6.25rem 5rem;
 	}
 `
 
@@ -27,7 +27,7 @@ export const ClientsSectionWrapper = styled.div`
 
 	@media only screen and (min-width: ${breakpoints.lg}) {
 		padding-inline: 1rem;
-        padding-block: initial;
+		padding-block: initial;
 	}
 `
 
@@ -72,53 +72,53 @@ export const TitleAndButtonSection = styled.div`
 `
 
 export const TitleLine = styled.span`
-    width: 10.25rem;
-    height: 0.5rem;
-    background-color: ${colors.primary.normal[500]};
+	width: 10.25rem;
+	height: 0.5rem;
+	background-color: ${colors.primary.normal[500]};
 `
 
 export const BrandingsWrapper = styled.div`
-    --gap: 2.5rem;
+	--gap: 2.5rem;
 
 	display: flex;
 	flex-wrap: wrap;
-    gap: var(--gap);
-    max-width: 66.5rem;
-    width: 100%;
-    align-self: center;
+	gap: var(--gap);
+	max-width: 66.5rem;
+	width: 100%;
+	align-self: center;
 
-    @media only screen and (min-width: ${breakpoints.lg}) {
-        gap: 6.25rem var(--gap);
-    }
+	@media only screen and (min-width: ${breakpoints.lg}) {
+		gap: 6.25rem var(--gap);
+	}
 
 	.container-img {
-        flex: 1 1 calc(50% - var(--gap));
-        display: flex;
-        align-items: center;
-        justify-content: center;
+		flex: 1 1 calc(50% - var(--gap));
+		display: flex;
+		align-items: center;
+		justify-content: center;
 
-        @media only screen and (min-width: 488px) {
-            flex: 1 1 calc(33% - var(--gap));
-        }
+		@media only screen and (min-width: 488px) {
+			flex: 1 1 calc(33% - var(--gap));
+		}
 	}
 
 	.branding-img {
 		width: 100%;
-        height: 100%;
+		height: 100%;
 		max-height: 3.25rem;
-        object-fit: contain;
+		object-fit: contain;
 		opacity: 0.5;
 		filter: grayscale(100%);
-        cursor: pointer;
-        transition: all 0.2s ease-in-out;
+		cursor: pointer;
+		transition: all 0.2s ease-in-out;
 
-        &:hover{
-            opacity: 1;
-            filter: grayscale(0);
-        }
+		&:hover {
+			opacity: 1;
+			filter: grayscale(0);
+		}
 
-        @media only screen and (min-width: ${breakpoints.lg}) {
-            max-height: 3.75rem;
-        }
+		@media only screen and (min-width: ${breakpoints.lg}) {
+			max-height: 3.75rem;
+		}
 	}
 `
