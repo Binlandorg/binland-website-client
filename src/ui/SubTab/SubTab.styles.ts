f9dcc441edd07445de6cd21266ee0c1f438e5a52
mardecera <18353569+mardecera@users.noreply.github.com>
2025-06-24 11:31:15 -0500

    feat: add extra files

--- src/ui/SubTab/SubTab.styles.ts
+++ src/ui/SubTab/SubTab.styles.ts
@@ -4,7 +4,7 @@ import { FontWeight } from "styles/typography"
 import { Props } from "./SubTab.types"
 
 export const SubTabWrapper = styled.p<Props>`
-    font-weight: ${({ $weight = "semibold" }: Props) => FontWeight[$weight]};
-    font-size: clamp(0.625rem, 0.4145rem + 1.0526vw, 1.125rem);
-    line-height: 1.5;
+	font-weight: ${({ $weight = "semibold" }: Props) => FontWeight[$weight]};
+	font-size: clamp(0.625rem, 0.4145rem + 1.0526vw, 1.125rem);
+	line-height: 1.5;
 `
