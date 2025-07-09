487de25eff1692ee2fb98ce429cce0e07036b288
mardecera <18353569+mardecera@users.noreply.github.com>
2025-07-09 10:45:41 -0500

    chore: correct business name and fiscal address text to match SUNAT records

--- src/context/constants/constans.ts
+++ src/context/constants/constans.ts
@@ -1,8 +1,9 @@
 export const COMPANY_CONTACT_EMAIL = "contact@binland.dev"
 export const COMPANY_PHONE_NUMBER = "953020555"
 export const COMPANY_RUC = "20613035053"
-export const COMPANY_REGISTERED_ADDRESS = "Av. Arequipa 1234, Arequipa, Perú"
-export const COMPANY_LEGAL_NAME = "Binland S.A.C"
+export const COMPANY_REGISTERED_ADDRESS =
+	"MZA. A LOTE. 4 URB. SANTA MARIA II AREQUIPA - AREQUIPA - JOSE LUIS BUSTAMANTE Y RIVERO"
+export const COMPANY_LEGAL_NAME = "BINLAND S.A.C."
 
 /** Country phone codes */
 export const PERU_PHONE_CODE = "+51"
