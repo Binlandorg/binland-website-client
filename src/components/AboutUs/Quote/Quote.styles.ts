69b898fda5b978d895a08d28b154f42e8638f842
mardecera <18353569+mardecera@users.noreply.github.com>
2025-07-15 16:15:44 -0500

    fix: add extra files

--- src/components/AboutUs/Quote/Quote.styles.ts
+++ src/components/AboutUs/Quote/Quote.styles.ts
@@ -5,251 +5,261 @@ import { FontWeight } from "styles/typography"
 import H1 from "ui/Titles/H1"
 
 export const HeroWrapper = styled.div`
-    --color: ${colors.body};
-    --size: 1rem;
-    --border-radius: calc(var(--size) * 0.5);
+	--color: ${colors.body};
+	--size: 1rem;
+	--border-radius: calc(var(--size) * 0.5);
 
-    display: flex;
-    flex-direction: column;
-    gap: 0.75rem;
-    margin-top: 1.5rem;
+	display: flex;
+	flex-direction: column;
+	gap: 0.75rem;
+	margin-top: 1.5rem;
 
-    @media only screen and (min-width: 900px) {
-        --size: 2rem;
+	@media only screen and (min-width: 900px) {
+		--size: 2rem;
 
-        margin-top: 5rem;
-        gap: initial;
-    }
+		margin-top: 5rem;
+		gap: initial;
+	}
 `
 
 export const HeroText = styled.div<{ $heightDiff: number }>`
-    --fix-size: min(${props => Math.round(props.$heightDiff / 2)}px, var(--size));
+	--fix-size: min(
+		${(props) => Math.round(props.$heightDiff / 2)}px,
+		var(--size)
+	);
 
-    @media only screen and (min-width: 900px) {
-        display: flex;
-        position: relative;
-        z-index: 2;
+	@media only screen and (min-width: 900px) {
+		display: flex;
+		position: relative;
+		z-index: 2;
 
-        & .title, & .text{
-            flex: 1;
-        }
+		& .title,
+		& .text {
+			flex: 1;
+		}
 
-        & .title{
-            background-color: var(--color);
-            height: fit-content;
-            padding-bottom: 1rem;
-            border-bottom-right-radius: var(--border-radius);
-            position: relative;
-            padding-right: 2rem;
+		& .title {
+			background-color: var(--color);
+			height: fit-content;
+			padding-bottom: 1rem;
+			border-bottom-right-radius: var(--border-radius);
+			position: relative;
+			padding-right: 2rem;
 
-            &.padding-none{
-                padding-right: initial;
-            }
+			&.padding-none {
+				padding-right: initial;
+			}
 
-            & .display-none{
-                display: none;
-            }
+			& .display-none {
+				display: none;
+			}
 
-            & .border-radius-left-bottom, 
-            & .border-radius-right-bottom{
-                position: absolute;
-                background-color: transparent;
-                overflow: hidden;
+			& .border-radius-left-bottom,
+			& .border-radius-right-bottom {
+				position: absolute;
+				background-color: transparent;
+				overflow: hidden;
 
-                &::after{
-                    content: "";
-                    width: 100%;
-                    height: 100%;
-                    border-radius: var(--border-radius);
-                    background-color: transparent;
-                    position: absolute;
-                }
-            }
+				&::after {
+					content: "";
+					width: 100%;
+					height: 100%;
+					border-radius: var(--border-radius);
+					background-color: transparent;
+					position: absolute;
+				}
+			}
 
-            & .border-radius-left-bottom{
-                width: var(--size);
-                height: var(--size);
-                left: 0;
-                top: 100%;
+			& .border-radius-left-bottom {
+				width: var(--size);
+				height: var(--size);
+				left: 0;
+				top: 100%;
 
-                &::after{
-                    top: 0;
-                    left: 0;
-                    box-shadow: calc(var(--size) * -0.5) calc(var(--size) * -0.5) 0 0px var(--color);
-                }
-            }
+				&::after {
+					top: 0;
+					left: 0;
+					box-shadow: calc(var(--size) * -0.5) calc(var(--size) * -0.5) 0 0px
+						var(--color);
+				}
+			}
 
-            & .border-radius-right-bottom{
-                width: var(--fix-size);
-                height: var(--fix-size);
-                right: 0;
-                top: 100%;
+			& .border-radius-right-bottom {
+				width: var(--fix-size);
+				height: var(--fix-size);
+				right: 0;
+				top: 100%;
 
-                &::after{
-                    top: 0;
-                    left: 0;
-                    box-shadow: calc(var(--fix-size) * 0.5) calc(var(--fix-size) * -0.5) 0 0px var(--color);
-                }
-            }
-        }
+				&::after {
+					top: 0;
+					left: 0;
+					box-shadow: calc(var(--fix-size) * 0.5) calc(var(--fix-size) * -0.5) 0 0px
+						var(--color);
+				}
+			}
+		}
 
-        & .text{
-            background-color: var(--color);
-            height: fit-content;
-            padding-bottom: 2rem;
-            padding-left: 2rem;
-            border-bottom-left-radius: var(--border-radius);
-            position: relative;
+		& .text {
+			background-color: var(--color);
+			height: fit-content;
+			padding-bottom: 2rem;
+			padding-left: 2rem;
+			border-bottom-left-radius: var(--border-radius);
+			position: relative;
 
-            &.padding-none{
-                padding-left: initial;
-            }
+			&.padding-none {
+				padding-left: initial;
+			}
 
-            & .display-none{
-                display: none;
-            }
+			& .display-none {
+				display: none;
+			}
 
-            & .border-radius-right-bottom, .border-radius-left-bottom{
-                position: absolute;
-                background-color: transparent;
-                overflow: hidden;
+			& .border-radius-right-bottom,
+			.border-radius-left-bottom {
+				position: absolute;
+				background-color: transparent;
+				overflow: hidden;
 
-                &::after{
-                    content: "";
-                    width: 100%;
-                    height: 100%;
-                    border-radius: var(--border-radius);
-                    background-color: transparent;
-                    position: absolute;
-                }
-            }
+				&::after {
+					content: "";
+					width: 100%;
+					height: 100%;
+					border-radius: var(--border-radius);
+					background-color: transparent;
+					position: absolute;
+				}
+			}
 
-            & .border-radius-left-bottom{
-                width: var(--fix-size);
-                height: var(--fix-size);
-                top: 100%;
-                left: 0;
+			& .border-radius-left-bottom {
+				width: var(--fix-size);
+				height: var(--fix-size);
+				top: 100%;
+				left: 0;
 
-                &::after{
-                    top: 0;
-                    left: 0;
-                    box-shadow: calc(var(--fix-size) * -0.5) calc(var(--fix-size) * -0.5) 0 0px var(--color);
-                }
-            }
+				&::after {
+					top: 0;
+					left: 0;
+					box-shadow: calc(var(--fix-size) * -0.5) calc(var(--fix-size) * -0.5) 0 0px
+						var(--color);
+				}
+			}
 
-            & .border-radius-right-bottom{
-                width: var(--size);
-                height: var(--size);
-                top: 100%;
-                right: 0;
+			& .border-radius-right-bottom {
+				width: var(--size);
+				height: var(--size);
+				top: 100%;
+				right: 0;
 
-                &::after{
-                    top: 0;
-                    left: 0;
-                    box-shadow: calc(var(--size) * 0.5) calc(var(--size) * -0.5) 0 0px var(--color);
-                }
-            }
-        }
-    }
+				&::after {
+					top: 0;
+					left: 0;
+					box-shadow: calc(var(--size) * 0.5) calc(var(--size) * -0.5) 0 0px
+						var(--color);
+				}
+			}
+		}
+	}
 
-    @media only screen and (min-width: 900px) {
-        & .title {
-            max-width: min-content;
-        }
-    }
+	@media only screen and (min-width: 900px) {
+		& .title {
+			max-width: min-content;
+		}
+	}
 `
 
 export const H1Bold = styled(H1)`
 	font-weight: ${FontWeight.bold};
-    color: ${colors.black.main};
-    text-transform: uppercase;
+	color: ${colors.black.main};
+	text-transform: uppercase;
 
-    @media only screen and (min-width: 900px) {
-        width: min-content;
-        margin-top: -1.5rem;
-    }
+	@media only screen and (min-width: 900px) {
+		width: min-content;
+		margin-top: -1.5rem;
+	}
 `
 
-export const ImageContainer = styled.div<{$heightDiff: number}>`
-  
-    position: relative;
-    border-radius: var(--border-radius);
-    overflow: hidden;
-    margin-bottom: 2.5rem;
+export const ImageContainer = styled.div<{ $heightDiff: number }>`
+	position: relative;
+	border-radius: var(--border-radius);
+	overflow: hidden;
+	margin-bottom: 2.5rem;
 
-    & .overlay{
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 50%;
-        height: 15%;
-        background-color: var(--color);
-        border-bottom-right-radius: var(--border-radius);
-        z-index: 1;
+	& .overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 50%;
+		height: 15%;
+		background-color: var(--color);
+		border-bottom-right-radius: var(--border-radius);
+		z-index: 1;
 
-        &::after, &::before{
-            content: "";
-            position: absolute;
-            width: var(--size);
-            height: var(--size);
-            border-radius: var(--border-radius);
-            background-color:transparent;
-            box-shadow: calc(var(--size) * -0.5) calc(var(--size) * -0.5) 0 0px var(--color);
-        }
+		&::after,
+		&::before {
+			content: "";
+			position: absolute;
+			width: var(--size);
+			height: var(--size);
+			border-radius: var(--border-radius);
+			background-color: transparent;
+			box-shadow: calc(var(--size) * -0.5) calc(var(--size) * -0.5) 0 0px
+				var(--color);
+		}
 
-        &::after{
-            top: 100%;
-            left: 0;
-        }
+		&::after {
+			top: 100%;
+			left: 0;
+		}
 
-        &::before{
-            top: 0;
-            left: 100%;
-        }
-    }
+		&::before {
+			top: 0;
+			left: 100%;
+		}
+	}
 
-    @media only screen and (min-width: 900px) {
-        --size: 6rem;
+	@media only screen and (min-width: 900px) {
+		--size: 6rem;
 
-        margin-top: -${({$heightDiff}) => $heightDiff}px;
-        z-index: -1;
+		margin-top: -${({ $heightDiff }) => $heightDiff}px;
+		z-index: -1;
 
-        & .overlay{
-            display: none;
-        }
-    }
+		& .overlay {
+			display: none;
+		}
+	}
 `
 
 export const Image = styled.img`
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    scale: 1.2;
-    transform-origin: left top;
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	scale: 1.2;
+	transform-origin: left top;
 
-    @media only screen and (min-width: 900px) {
-        scale: initial;
-    }
+	@media only screen and (min-width: 900px) {
+		scale: initial;
+	}
 `
 
 export const QuoteWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    
-    & .quote-icon{
-        width: 2.25rem;
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
 
-        & path{
-            fill: ${colors.secondary.normal[300]};
-        }
-    }
+	& .quote-icon {
+		width: 2.25rem;
 
-    & .quote-text{
-        font-weight: ${FontWeight.regular};
-    }
+		& path {
+			fill: ${colors.secondary.normal[300]};
+		}
+	}
 
-    & .quote-icon:last-child{
-        align-self: end;
-    }
+	& .quote-text {
+		font-weight: ${FontWeight.regular};
+	}
+
+	& .quote-icon:last-child {
+		align-self: end;
+	}
 `
